509f554df6b014fb23f5a99fc01fd589b0ad3179
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2023-09-10 22:00:09 +0100

    feat: add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 import useSWR from 'swr';
 import { fetchAllLists } from './api'; // Import the fetchAllLists function and types
 import { Task, List, ListWithTasks } from './types';
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  // Example: Fetch a list of lists
   const { data, error } = useSWR<ListWithTasks[]>('/api/lists', fetchAllLists);
 
   if (error) return <div>Error loading data</div>;
@@ -26,6 +26,7 @@ export default function Home() {
         <div key={list.id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
           <h2>{list.name}</h2>
           {renderTasks(list.tasks)}
+          <Button >Hello</Button>
         </div>
       ))}
     </div>
